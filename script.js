7a24281342a2d26be2637258077f2a0297d5231a
Mario Rovito <alessandro.r14@gmail.com>
2022-03-15 15:42:27 +0100

    add numbers inside the cell

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ const rows = 10;
 // numero totale delle celle è dato dalla moltiplicazione di colonne + righe
 const totalCells = columns * rows;
 
+//creo una funzione che mi permetta di creare le mie celle 
+
+createCell = () =>{
+    const item = document.createElement('div');
+    item.classList.add('cell');
+    return item;
+}
 
 // creo il for con all'interno la funzione per creare le celle e verifico se funziona
 for(let i = 0; i < totalCells; i++){
@@ -21,14 +28,13 @@ for(let i = 0; i < totalCells; i++){
     const cell = createCell();
     
     grid.appendChild(cell);
+
+// inserisco all'interno delle celle i numeri, +1 lo inserisco così da non farlo partire da 0 ma da 1
+    cell.innerText = (i +1);
     }
 
 
-//creo una funzione che mi permetta di creare le mie celle 
 
- createCell = () =>{
-    const item = document.createElement('div');
-    item.classList.add('cell');
-    return item;
-}
+
+
 
