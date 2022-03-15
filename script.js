4d3a681f213f570fb04f36182078f0f60e7c0ae5
Mario Rovito <alessandro.r14@gmail.com>
2022-03-15 15:09:26 +0100

    add colums rows and cells in js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('JS OK!')
+
+
+//creo una griglia 10 x 10
+const grid = document.getElementById ('grid');
+
+//numero delle colonne = 10
+const columns = 10;
+
+// numero delle righe = 10
+const rows = 10;
+
+// numero totale delle celle è dato dalla moltiplicazione di colonne + righe
+const totalCells = columns * rows;
+
