a3d69047401379d43172018dcc36f6264a168050
Mario Rovito <alessandro.r14@gmail.com>
2022-03-15 15:31:27 +0100

    add function for create cell

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ const rows = 10;
 // numero totale delle celle è dato dalla moltiplicazione di colonne + righe
 const totalCells = columns * rows;
 
+
+// creo il for con all'interno la funzione per creare le celle e verifico se funziona
+for(let i = 0; i < totalCells; i++){
+
+
+    const cell = createCell();
+    
+    grid.appendChild(cell);
+    }
+
+
+//creo una funzione che mi permetta di creare le mie celle 
+
+ createCell = () =>{
+    const item = document.createElement('div');
+    item.classList.add('cell');
+    return item;
+}
+
